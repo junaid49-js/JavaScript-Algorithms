a75314e64a6c5ce863bf944855b7cbdb5c0bf365
Junaid <156448521+junaid49-js@users.noreply.github.com>
2025-06-06 01:24:14 +0530

    Create isPrimeOrNot_better.js

---
+++ PrimeNumber/isPrimeOrNot_better.js
@@ -0,0 +1,11 @@
+const isPrime = function(n){
+  if (n < 2){
+      return false;
+  }
+  for(let i = 2; i < n; i++){
+      if(n % i === 0){
+          return false;
+      }
+  }
+  return true
+}
