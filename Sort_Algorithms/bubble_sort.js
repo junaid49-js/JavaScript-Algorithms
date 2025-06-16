d3ba70d1f1c46a023111f5ba7d36f84a97ac4554
Junaid <156448521+junaid49-js@users.noreply.github.com>
2025-06-17 01:26:55 +0530

    Update bubble_sort.js
    Added comments so I can remember how it works.

--- Sort_Algorithms/bubble_sort.js
+++ Sort_Algorithms/bubble_sort.js
@@ -1,16 +1,30 @@
 const bubbleSort = (array) => {
+    // Declare a variable to track if any element is swapped
     let isSwapped;
+    // do while loop, we need to run the program atleast once to check if array is sorted or not, if isSwapped remains false it means the array is sorted
     do{
+        // Initialize isSwapped to false
         isSwapped = false;
-    for(let i = 0; i < array.length - 1; i++){
-        if(array[i] > array[i+1]){
-            let temp = array[i];
-            array[i] = array[i+1];
-            array[i+1] = temp;
-            isSwapped = true;    
+        // Iterating the array
+        for(let i = 0; i < array.length - 1; i++){
+            // Checking if an element is larger than the element after it
+            if(array[i] > array[i+1]){
+                // If the if statement is true then this block of code will execute
+                // Declaring and initializing a temporary variable to hold the current number
+                let temp = array[i];
+                // Swapping the current element with the smaller element
+                // Assigning the smaller element in the place of the current element
+                array[i] = array[i+1];
+                // Assigning the temporary variable which holds the larger element to the next element
+                array[i+1] = temp;
+                // Since this block of code ran and we swapped two elements, we will assign the isSwapped variable to true value
+                isSwapped = true;    
+            }
         }
-    }
+        // The condition on what the do while loop will continue to execute
+        // If the swapping happens the do while loop will execute again
     } while(isSwapped)
 
+    // after all of that we return our sorted array    
     return array
 }
