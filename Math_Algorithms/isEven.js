5db13331178bcb82ee25fdf2f4c2fce84674d78c
Junaid <156448521+junaid49-js@users.noreply.github.com>
2025-07-09 00:00:29 +0530

    Update isEven.js

--- Math_Algorithms/isEven.js
+++ Math_Algorithms/isEven.js
@@ -1,6 +1,9 @@
+// Declare a function with number to check if it is an even number as an argument
 const isEven = (number) => {
+  // if remainder of the number divided by 2 is 0 than the number is even
   if(number % 2 === 0){
     return true;
   }
+  // if none of condition matches than the number is not even
  return false;
 }
