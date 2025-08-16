7c8e46f246f4d4c5c55d1fc19fece09a0b9d8a76
Junaid <156448521+junaid49-js@users.noreply.github.com>
2025-08-16 13:29:44 +0530

    Create maxSubArraySum.js

---
+++ Some_Questions/maxSubArraySum.js
@@ -0,0 +1,18 @@
+function maxSubArraySum(arr, k){
+    if(arr.length < k) return null;
+
+    let maxSum = 0;
+    let windowSum = 0;
+
+    for(let i = 0; i < k; i++){
+        windowSum += arr[i];
+    }
+    maxSum = windowSum;
+
+    for (let i = k; i < arr.length; i++){
+        windowSum += arr[i] - arr[i - k];
+        maxSum = Math.max(maxSum, windowSum);
+    }
+
+    return maxSum;
+}
